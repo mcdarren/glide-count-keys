223f103b51c6bdbd9cbc82489ad9418d6d1ded3f
Darren Murphy <darren@victoriajd.com>
2021-08-26 18:06:47 +0800

    * Fix incorrect parameter references

--- function.js
+++ function.js
@@ -3,8 +3,8 @@ window.function = function (list, keys) {
   if (list.value === undefined) return undefined;
   if (keys.value === undefined) return undefined;
   
-  var str_arr = list.split(',');
-  var keys_arr = keys.split(',');
+  var str_arr = list.value.split(',');
+  var keys_arr = keys.value.split(',');
   var count = 0;
   str_arr.forEach(function (code) {
     if (keys_arr.includes(code)) {
