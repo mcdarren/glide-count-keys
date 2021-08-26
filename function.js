62e092ebdb9ee7ae29e9df77def713bf56612773
Darren Murphy <darren@victoriajd.com>
2021-08-26 17:51:45 +0800

    Initial commit

---
+++ function.js
@@ -0,0 +1,16 @@
+
+window.function = function (data, sort_keys, delimiter) {
+  if (list.value === undefined) return undefined;
+  if (keys.value === undefined) return undefined;
+  
+  var str_arr = list.split(',');
+  var keys_arr = keys.split(',');
+  var count = 0;
+  str_arr.forEach(function (code) {
+    if (keys_arr.includes(code)) {
+      count++;
+    }
+  });
+  return count;
+
+}
