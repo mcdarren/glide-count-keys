f70f62bf2987c3921fadd2a5ebf3cb704cfaa9b0
Darren Murphy <darren@victoriajd.com>
2021-08-30 20:52:14 +0800

    * Allow for specification of number of items to consider

--- function.js
+++ function.js
@@ -1,9 +1,11 @@
 
-window.function = function (list, keys) {
+window.function = function (list, keys, items) {
   if (list.value === undefined) return undefined;
   if (keys.value === undefined) return undefined;
+
+  var items = defined items.value ? items.value : list.value.length;
   
-  var str_arr = list.value.split(',');
+  var str_arr = list.value.split(',').splice(0,items);
   var keys_arr = keys.value.split(',');
   var count = 0;
   str_arr.forEach(function (code) {
@@ -12,5 +14,4 @@ window.function = function (list, keys) {
     }
   });
   return count;
-
 }
