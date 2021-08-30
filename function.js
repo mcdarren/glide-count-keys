4e6456a6033c93a3e32a804531b3f3594e8c4bfd
Darren Murphy <darren@victoriajd.com>
2021-08-30 21:24:18 +0800

    fix one more time...

--- function.js
+++ function.js
@@ -2,10 +2,10 @@
 window.function = function (list, keys, items) {
   if (list.value === undefined) return undefined;
   if (keys.value === undefined) return undefined;
+  if (items.value === undefined) return undefined;
 
   var str_arr = list.value.split(',');
-  var items = typeof items.value === 'undefined' ? str_arr.length : items.value;
-  var codes = str_arr.splice(0,items);
+  var codes = str_arr.slice(0,items);
   var keys_arr = keys.value.split(',');
   var count = 0;
   codes.forEach(function (code) {
